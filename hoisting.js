cf9301fbfceda4e058101693e629de9ad6a569e8
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-06-08 10:35:27 +0530

    tdz

---
+++ hoisting.js
@@ -0,0 +1,26 @@
+// var a
+//function hello(){
+//     console.log("Hello World")
+// }
+// let a
+
+
+// var a = 10
+
+
+
+// function hello(){
+//     console.log("Hello World")
+// }
+
+
+// is let and const also hoisted?
+
+// let a
+
+console.log(a)
+
+
+
+
+let a = 100
---
+++ tdz.js
@@ -0,0 +1,23 @@
+// temporal dead zone
+// let a
+// let a = 100
+
+
+
+
+
+function hello(){
+    // let age
+     
+     function printAge(age){
+            console.log(age)
+     }
+
+
+     let age = 30 
+     printAge(age)
+
+}
+
+
+hello()
